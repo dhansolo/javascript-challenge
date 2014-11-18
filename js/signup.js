59493d9809afc2fad86a317e3af937dd5473df88
dhansolo <dhansolo@uw.edu>
2014-11-17 21:45:03 -0800

    final version

--- js/signup.js
+++ js/signup.js
@@ -3,7 +3,7 @@
 function load() {
 	var selectState = document.getElementsByName("state");
 	for (var i = 0; i < usStates.length; i++) {
-		var opt = document.createElement("OPTION");
+		var opt = document.createElement("option");
 		opt.text = usStates[i].name;
 		opt.value = usStates[i].code;
 		selectState[0].appendChild(opt);
@@ -35,63 +35,75 @@ function load() {
 }
 
 function onSubmit(evt) {
-    var valid = true;
-    if (!valid && evt.preventDefault) {
-        evt.preventDefault();
-    }
+    var valid;
     try {
 		valid = validate(this);
 	} catch(exception) {
 		console.log(exception);
 		valid = false; 
 	}
+	if (!valid && evt.preventDefault) {
+        evt.preventDefault();
+    }
     evt.returnValue = valid;
     return valid;
 } 
 
-//
 function validate(form) {
     var required = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
-    var index;
     var valid = true;
+    var index;
     if (document.getElementsByName('occupationOther')[0].style.display == 'inline') {
 		required.push('occupationOther');
 	}
-    for(index = 0; index < required.length; index++) {
-    	var input = document.getElementsByName(required[index])[0];
-        if(input.value === "") {
-        	input.className = 'form-control invalid-field';
-        	valid = false;
-       	} else {
-       		input.className = 'form-control';
-       	}
-       	if(input === 'zip') {
-       		var zipRegExp = new RegExp('^\\d{5}$');
-       		if(!zipRegExp.test(input.value)) {
-       			input.className = 'form-control invalid-field';
-       			valid = false;
-       		} else {
-       			input.className = 'form-control';
-       		}
-       	}
-       	if(input === 'birthdate') {
-       		var birthday = new Date(input.value);
-       		var currentDate = new Date();
-       	 	if (input.value === "") {
-				input.className = 'form-control invalid-field';
-				valid = false;
-			} else if (!(currentDate.getDay() <= birthday.getDay() && currentDate.getMonth() <= birthday.getMonth() && currentDate.getFullYear() - 13 >= birthday.getFullYear())) {
-				document.getElementById('birthdateMessage').innerHTML = "Must be 13 years of age";
-				document.getElementById('birthdateMessage').className = '';
-				input.className = 'form-control invalid-field';
-				valid = false;
-			} else {
-				document.getElementById('birthdateMessage').innerHTML = "";
-				document.getElementById('birthdateMessage').className = "";
-				input.className = 'form-control';
-			}
-       	}
-    }
-    return valid;
+	for(index = 0; index < required.length; index++) {
+		valid = validateForm(required[index], form);
+	}
+	return valid;
+}
+
+
+function validateForm(field, form) {
+	var zipRegExp = new RegExp('^\\d{5}$');
+	var age = 13;
+	var old = true;
+	var realZip = true;
+	if(field === 'zip') {
+		var ziptest = zipRegExp.test(form[field].value);
+		if(!ziptest) {
+			realZip = false;
+		}
+	}
+	if(field === 'birthdate') {
+		var birthday = new Date(form[field].value);
+		var currentDate = new Date();
+		var yearDifference = currentDate.getFullYear() - birthday.getFullYear();
+		var monthDifference = currentDate.getMonth() - birthday.getMonth();
+		if(currentDate.getDate() <= birthday.getDate() && monthDifference === 0 || monthDifference < 0) {
+			yearDifference = yearDifference - 1;
+		}
+		if(yearDifference < age) {
+			old = false;
+		}
+	}
+	if(form[field].value.trim().length === 0 || field === 'zip' && !realZip || field === 'birthdate' && !old) {
+	    form[field].className = 'form-control invalid-field';
+	    if(field === 'birthdate' && !old) {
+			document.getElementById("birthdateMessage").innerHTML = 'Sorry, you must be 13 years of age to sign up';
+	    }
+	    if(field === 'zip' && !realZip) {
+	    	document.getElementById("zipMessage").innerHTML = 'Must be 5 digits!';
+	    }
+	    return false;
+	} else {
+		form[field].className = 'form-control';
+		if(old === true) {
+			document.getElementById("birthdateMessage").innerHTML = '';
+	    }
+	    if(realZip === true) {
+	    	document.getElementById("zipMessage").innerHTML = '';
+	    }
+	    return true;
+	}
 }
 document.addEventListener('DOMContentLoaded', load);
